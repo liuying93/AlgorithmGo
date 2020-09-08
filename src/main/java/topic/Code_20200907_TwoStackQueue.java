ae32162a991ba06ef3fab678f27ef5fdc0512ac4
liuying <liuying@fuiou.com>
2020-09-08 17:23:42 +0800

    two stack queue

--- src/main/java/topic/Code_20200907_TwoStackQueue.java
+++ src/main/java/topic/Code_20200907_TwoStackQueue.java
@@ -1,5 +1,8 @@
 package main.java.topic;
 
+
+import java.util.Stack;
+
 /**
  * 由两个栈组成的队列
  *
@@ -9,5 +12,50 @@ package main.java.topic;
  */
 public class Code_20200907_TwoStackQueue {
 
+    private Stack<Integer> pushStack;
+    private Stack<Integer> popStack;
 
+    public Code_20200907_TwoStackQueue() {
+        pushStack = new Stack<Integer>();
+        popStack = new Stack<Integer>();
+    }
+
+    public void add(int num) {
+        pushStack.push(num);
+    }
+
+    public int poll() {
+        if (pushStack.isEmpty() && popStack.isEmpty()) {
+            throw new RuntimeException("Stack is Empty");
+        } else if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.pop();
+    }
+
+    public int peek() {
+        if (pushStack.isEmpty() && popStack.isEmpty()) {
+            throw new RuntimeException("Stack is Empty");
+        } else if (popStack.isEmpty()) {
+            while (!pushStack.isEmpty()) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        return popStack.peek();
+    }
+
+   /* public static void main(String[] args) {
+        Code_20200907_TwoStackQueue obj = new Code_20200907_TwoStackQueue();
+        int[] arr = {3, 1, 4, 5, 6, 1, 0, 4};
+        for (int i = 0; i < arr.length; i++) {
+            obj.add(arr[i]);
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Min:" + obj.peek());
+            System.out.println("Pop:" + obj.poll());
+        }
+    }*/
 }
