2042799e5290b33a456add3a0719d92b39084743
liuying <liuying@fuiou.com>
2020-09-15 17:06:32 +0800

    stackorderstack

--- src/main/java/topic/Code_20200914_StackOrderStack.java
+++ src/main/java/topic/Code_20200914_StackOrderStack.java
@@ -11,29 +11,36 @@ import java.util.Stack;
  */
 public class Code_20200914_StackOrderStack {
 
-   /* private Stack stack;
-    private Stack help;
-
-    public Code_20200914_StackOrderStack() {
-        this.stack = new Stack();
-        this.help = new Stack();
-    }*/
-
-
     public static void sortStackByStack(Stack<Integer> stack) {
         if (null == stack || stack.size() < 2) {
             return;
         }
 
         Stack<Integer> help = new Stack();
-        int curr = stack.pop();
-        help.push(curr);
         while (!stack.isEmpty()) {
-
-
+            int curr = stack.pop();
+            while (!help.isEmpty() && curr > help.peek()) {
+                stack.push(help.pop());
+            }
+            help.push(curr);
         }
 
+        while (!help.isEmpty()) {
+            stack.push(help.pop());
+        }
+    }
 
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack();
+        int[] arr = {3, 1, 4, 5, 6, 1, 0, 4};
+        for (int i = 0; i < arr.length; i++) {
+            stack.push(arr[i]);
+        }
+        sortStackByStack(stack);
+        while (!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
     }
 
 }
