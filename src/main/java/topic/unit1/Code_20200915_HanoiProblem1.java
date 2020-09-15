91b0b90680993d8765c9e501a09b81f6e65698f3
liuying <liuying@fuiou.com>
2020-09-15 17:20:12 +0800

    move

---
+++ src/main/java/topic/unit1/Code_20200915_HanoiProblem1.java
@@ -0,0 +1,13 @@
+package main.java.topic.unit1;
+
+/**
+ * 汉诺塔问题
+ * 递归方式求解
+ *
+ * @Author: liuy
+ * @Date: 2020/9/15 17:13
+ * @Description:
+ */
+public class Code_20200915_HanoiProblem1 {
+
+}
--- src/main/java/topic/unit1/Code_20200915_HanoiProblem2.java
+++ src/main/java/topic/unit1/Code_20200915_HanoiProblem2.java
@@ -2,12 +2,12 @@ package main.java.topic.unit1;
 
 /**
  * 汉诺塔问题
- * 递归方式求解
+ * 压栈方式求解
  *
  * @Author: liuy
- * @Date: 2020/9/15 17:13
+ * @Date: 2020/9/15 17:18
  * @Description:
  */
-public class Code_20200915_hanoiProblem1 {
+public class Code_20200915_HanoiProblem2 {
 
 }
