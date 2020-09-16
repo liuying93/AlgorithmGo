8981a226a595135e2cf49760ab4f2fe6f5174175
liuying <liuying@fuiou.com>
2020-09-16 17:31:40 +0800

    hanoi

--- src/main/java/topic/unit1/Code_20200915_HanoiProblem1.java
+++ src/main/java/topic/unit1/Code_20200915_HanoiProblem1.java
@@ -10,4 +10,53 @@ package main.java.topic.unit1;
  */
 public class Code_20200915_HanoiProblem1 {
 
+    public static void main(String[] args) {
+        int s = hanoiProblem1(4, "Left", "Right", "Mid");
+        System.out.println(s);
+    }
+
+
+    public static int hanoiProblem1(int num, String left, String right, String mid) {
+        if (num < 1) {
+            return 0;
+        }
+
+        int result = process(num, left, right, mid, left, right);
+        return result;
+    }
+
+
+    private static int process(int num, String left, String right, String mid, String from, String to) {
+        if (num == 1) {
+            if (mid.equals(from) || mid.equals(to)) {
+                System.out.println("Move  1  from " + from + " to " + to);
+                return 1;
+            } else {
+                System.out.println("Move  1  from " + from + " to " + mid);
+                System.out.println("Move  1  from " + mid + " to " + to);
+                return 2;
+            }
+        }
+
+        if (mid.equals(from) || mid.equals(to)) {
+            String another = (left.equals(from) || left.equals(to)) ? right : left;
+            int step1 = process(num - 1, left, right, mid, from, another);
+            System.out.println("Move " + num + "  from  " + from + " to " + mid);
+            int step2 = 1;
+            int step3 = process(num - 1, left, right, mid, another, to);
+            return step1 + step2 + step3;
+        } else {
+            int step1 = process(num - 1, left, right, mid, from, to);
+            System.out.println("Move " + num + "  from  " + from + " to " + mid);
+            int step2 = 1;
+            int step3 = process(num - 1, left, right, mid, to, from);
+            System.out.println("Move " + num + "  from  " + mid + " to " + to);
+            int step4 = 1;
+            int step5 = process(num - 1, left, right, mid, from, to);
+            return step1 + step2 + step3 + step4 + step5;
+        }
+
+
+    }
+
 }
--- src/main/java/topic/unit1/Code_20200915_HanoiProblem2.java
+++ src/main/java/topic/unit1/Code_20200915_HanoiProblem2.java
@@ -1,5 +1,7 @@
 package main.java.topic.unit1;
 
+import java.util.Stack;
+
 /**
  * 汉诺塔问题
  * 压栈方式求解
@@ -10,4 +12,48 @@ package main.java.topic.unit1;
  */
 public class Code_20200915_HanoiProblem2 {
 
+
+    public static void main(String[] args) {
+        int s = hanoiProblem2(4, "Left", "Right", "Mid");
+        System.out.println(s);
+    }
+
+
+    private enum Action {
+        No, LtoM, MtoL, RtoM, MtoR;
+    }
+
+    public static int hanoiProblem2(int num, String left, String right, String mid) {
+        Stack<Integer> LS = new Stack<>();
+        Stack<Integer> MS = new Stack<>();
+        Stack<Integer> RS = new Stack<>();
+
+        LS.push(Integer.MAX_VALUE);
+        MS.push(Integer.MAX_VALUE);
+        RS.push(Integer.MAX_VALUE);
+
+        for (int i = num; i > 0; i--) {
+            LS.push(i);
+        }
+
+        Action[] record = {Action.No};
+        int step = 0;
+        while (RS.size() < num + 1) {
+            step += fStackTotStack(record, Action.MtoL, Action.LtoM, LS, MS, left, mid);
+            step += fStackTotStack(record, Action.LtoM, Action.MtoL, MS, LS, mid, left);
+            step += fStackTotStack(record, Action.RtoM, Action.MtoR, MS, RS, mid, right);
+            step += fStackTotStack(record, Action.MtoR, Action.RtoM, RS, MS, left, mid);
+        }
+        return step;
+    }
+
+    public static int fStackTotStack(Action[] record, Action preNoAction, Action nowAction, Stack<Integer> fS, Stack<Integer> tS, String from, String to) {
+        if (record[0] != preNoAction && fS.peek() < tS.peek()) {
+            tS.push(fS.pop());
+            record[0] = nowAction;
+            System.out.println("Move " + tS.peek() + " from " + from + " to " + to);
+            return 1;
+        }
+        return 0;
+    }
 }
